2d354b1153ea72b39f28261bd4727852781c04b1
javier23900 <61750453+javier23900@users.noreply.github.com>
2020-03-10 23:02:37 +0100

    Add files via upload

--- PADSOF/src/ads/PADSOF/Aplicacion.java
+++ PADSOF/src/ads/PADSOF/Aplicacion.java
@@ -1,13 +1,63 @@
 package ads.PADSOF;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aplicacion {
 
 	public int umbral;
-	
+
+	private static List<Ciudadano> usuariosAceptados = new ArrayList<>();
+
+    private static List<Ciudadano> usuariosPorAceptar = new ArrayList<>();
+    
+    private static List<Colectivo> colectivos = new ArrayList<>();
+    
+    private static List<Proyecto> proyectosAceptados = new ArrayList<>();
+    
+    private static List<Proyecto> proyectosPorAceptar = new ArrayList<>();
+
 	public Aplicacion(int limite) {
 		umbral = limite;
 
 	  }
+	
+
+	public static List<Ciudadano> getUsuariosAceptados() {
+		return usuariosAceptados;
+	}
+
+
+
+	public static void setUsuariosAceptados(List<Ciudadano> usuariosAceptados) {
+		Aplicacion.usuariosAceptados = usuariosAceptados;
+	}
+
+
+
+	public static List<Ciudadano> getUsuariosPorAceptar() {
+		return usuariosPorAceptar;
+	}
+
+
+
+	public static void setUsuariosPorAceptar(List<Ciudadano> usuariosPorAceptar) {
+		Aplicacion.usuariosPorAceptar = usuariosPorAceptar;
+	}
+
+
+
+	public static List<Colectivo> getColectivos() {
+		return colectivos;
+	}
+
+
+
+	public static void setColectivos(List<Colectivo> colectivos) {
+		Aplicacion.colectivos = colectivos;
+	}
+
+
 
 	public int getUmbral() {
 		return umbral;
@@ -16,18 +66,19 @@ public class Aplicacion {
 	public void setUmbral(int umbral) {
 		this.umbral = umbral;
 	}
-	
+
 	public void login (String user, String contrasenia) {
-		
+
 	}
-	
+
 	public void logout () {
+
+	}
+
+	public Notificacion generarNotificacion(String titulo) {
 		
 	}
-	
-	public Notificacion generarNotificacion() {
-		
-	}
-	
-	
+
+
+
 }
